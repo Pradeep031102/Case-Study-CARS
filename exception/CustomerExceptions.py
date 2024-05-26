3f717589628536603ddb7406182f62c295c08827
pradeep031102 <pradeepssundaraganesh@gmail.com>
2024-05-26 22:34:19 +0530

    'update'

--- exception/CustomerExceptions.py
+++ exception/CustomerExceptions.py
@@ -1,4 +1,5 @@
 class IncidentNumberNotFoundException(Exception):
     def __init__(self, message="Incident number not found"):
+        #calling the variable
         self.message = message
         super().__init__(self.message)
--- unit_test.py
+++ unit_test.py
@@ -2,48 +2,57 @@ import unittest
 from unittest.mock import MagicMock
 from dao.CrimeAnalysisServiceImpl import CrimeAnalysisServiceImpl
 from entity.Incident import  Incident
-from util.DBConnUtil import DBConnUtil
 
-from datetime import datetime
 
 
 class TestCrimeAnalysisService(unittest.TestCase):
+    def setUp(self):
+        self.service=CrimeAnalysisServiceImpl()
+
+    def test_add_incident(self):
+        incidentType= "Homicide"
+        incidentDate= '2024-04-29'
+        Location_Longitude= 36.9028
+        Location_Latitude= -26.6458
+        description= "Homicide attack occurred at the bank."
+        status= "Under Investigation"
+        victimID= 103
+        suspectID= 1005
+        self.test_incident = Incident(None, incidentType, incidentDate,Location_Longitude,Location_Latitude,description,status,victimID,suspectID)
+        self.test_incident_id = self.service.createIncident(self.test_incident)
+        self.assertIsNotNone(self.test_incident_id)
+
     def test_create_incident_success(self):
-        # Mock user input
+       
         user_input = {
-            "incident_id":15 ,
+            "incident_id": 16,
             "incident_type": "Theft",
-            "incident_date": '2024-04-15',
-            "location_longitude": 45.9078,
-            "location_latitude": -24.7893,
-            "description": "Robbery",
+            "incident_date": '2024-04-21',
+            "location_longitude": 25.0920,
+            "location_latitude": -45.2763,
+            "description": "Theft of Gold",
             "status": "Open",
             "victim_id": 101,
             "suspect_id": 1002
         }
 
-        # Create an Incident object
         incident = Incident(**user_input)
 
-        # Create an instance of CrimeAnalysisServiceImpl
         service = CrimeAnalysisServiceImpl()
 
-        # Call the createIncident method with the incident object
         success = service.createIncident(incident)
 
-        # Assert that the method returns True (indicating successful creation)
         self.assertTrue(success)
     
     def test_update_incident_status_success(self):
-        # Mock user input
-        incident_id = 2
+        
+        incident_id = 1
         new_status = "Closed"
         service = CrimeAnalysisServiceImpl()
 
-        # Call the method
+ 
         success = service.updateIncidentStatus(new_status, incident_id)
 
-        # Assertions
         self.assertTrue(success)
         
 
